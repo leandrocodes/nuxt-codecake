5ea3e782e41b6ba292f52add3550eb46ec51f656
leandrocodes <leaxviana140@gmail.com>
2019-09-25 19:09:55 -0400

    starting

---
+++ components/AppBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app-bar">
+    <v-toolbar>
+      <v-toolbar-title>Code Cake</v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+
+      <v-toolbar-items>
+        <v-btn text>Link 1</v-btn>
+        <v-btn text>Link 2</v-btn>
+        <v-btn text>Link 3</v-btn>
+      </v-toolbar-items>
+
+      <template v-if="$vuetify.breakpoint.smAndUp">
+        <v-btn icon>
+          <v-icon>mdi-export-variant</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-delete-circle</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-plus-circle</v-icon>
+        </v-btn>
+      </template>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>>
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,6 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <div class="text-center">
-        <logo />
-        <vuetify-logo />
       </div>
       <v-card>
         <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
@@ -42,13 +40,8 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
   components: {
-    Logo,
-    VuetifyLogo
   }
 }
 </script>
