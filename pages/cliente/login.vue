4d60b5c7c6caf95f0aed895fc8103397a8759006
leandrocodes <leaxviana140@gmail.com>
2019-09-25 23:33:34 -0400

    login buttons

--- pages/cliente/login.vue
+++ pages/cliente/login.vue
@@ -12,7 +12,12 @@
             <v-col cols="12" md="4">
               <v-text-field password v-model="user.password" :rules="passRules" label="Senha" required></v-text-field>
             </v-col>
-            <v-btn @click="login">teste</v-btn>
+          </v-row>
+          <v-row align="center" justify="end">
+            <v-col cols="6">
+              <v-btn @click="login" class="mr-5" color="secondary">Registrar</v-btn>
+              <v-btn @click="login" color="primary">Login</v-btn>
+            </v-col>
           </v-row>
         </v-container>
       </v-form>
@@ -43,19 +48,11 @@ export default {
       //if (user) this.$router.push('/cliente/formulario')
       //console.log(user)
     })
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        this.$router.go()
-      })
   },
   methods: {
     login() {
       //console.log(db)
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(this.user.email, this.user.password)
+      firebase.auth().signInWithEmailAndPassword(this.user.email, this.user.password)
         .then(() => {
           console.log('logou')
           this.$store.commit('increment')
