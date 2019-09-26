30d284c9abac7e5890df4ef23f56eda6f0c280b8
leandrocodes <leaxviana140@gmail.com>
2019-09-25 21:49:57 -0400

    starting with vuex

--- pages/cliente/login.vue
+++ pages/cliente/login.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="login">
     <v-container>
-        <v-form v-model="valid">
-          <v-container>
-            <v-row align="center" justify="center">
-              <v-col cols="12" md="4">
-                <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row align="center" justify="center">
-              <v-col cols="12" md="4">
-                <v-text-field password v-model="password" :rules="passRules" label="Senha" required></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-form>
+      <v-form v-model="valid">
+        <v-container>
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="4">
+              <v-text-field v-model="user.email" :rules="emailRules" label="E-mail" required></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="4">
+              <v-text-field password v-model="user.password" :rules="passRules" label="Senha" required></v-text-field>
+            </v-col>
+            <v-btn @click="login">teste</v-btn>
+          </v-row>
+        </v-container>
+      </v-form>
     </v-container>
   </div>
 </template>
@@ -23,17 +24,25 @@
 export default {
   data() {
     return {
+      user: { password: '', email: '', logged: false},
       valid: false,
-      password: '',
       passRules: [
         v => !!v || 'Digitar a senha é obrigatório',
         v => v.length > 6 || 'Senha deve conter ao menos 6 caracteres'
       ],
-      email: '',
       emailRules: [
         v => !!v || 'Digitar o E-mail é obrigatório',
         v => /.+@.+/.test(v) || 'Digite um endereço de E-mail válido'
       ]
+    }
+  },
+  created() {
+    console.log(this.$store.state)
+  },
+  methods: {
+    login() {
+      this.$store.commit('increment')
+      console.log(this.$store.state)
     }
   }
 }
